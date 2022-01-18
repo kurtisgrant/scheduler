38b0ee18c60d9c0d9a83defd08f73dbf43f00044
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-01-18 11:15:46 -0500

    Initial commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,19 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from 'classnames';
+
 export default function Button(props) {
-   return <></>;
+   const classes = classNames(
+      'button',
+      { "button--confirm": props.confirm },
+      { "button--danger": props.danger }
+   );
+   return <button
+      disabled={props.disabled}
+      className={classes}
+      onClick={props.onClick}
+   >
+      {props.children}
+   </button>;
 }
