8859ee33d9e052bb342ed5d21eec399e11967916
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-01-23 13:03:10 -0500

    Add confirm view for appointment component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -160,5 +161,12 @@ storiesOf("Appointment", module)
       interviewer={"Sylvia Palmer"}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message={"Delete the appointment?"}
+      onCancel={action("onCancel")}
+      onConfirm={action("onConfirm")}
     />
   ));
